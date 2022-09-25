897ea984b8e652bc529a11eaa5c99b73245407c9
Allan G Rizza <allan.rizza@pumpkintech.com.br>
2022-09-25 12:22:03 -0300

    Herdando Tarefa em SubTarefa

--- src/main/java/com/listatarefas/subtarefa/SubTarefa.java
+++ src/main/java/com/listatarefas/subtarefa/SubTarefa.java
@@ -1,5 +1,6 @@
 package com.listatarefas.subtarefa;
 
+import com.listatarefas.tarefa.Tarefa;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Table(name = "subtarefas")
 @Getter
 @Setter
-public class SubTarefa {
+public class SubTarefa extends Tarefa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
